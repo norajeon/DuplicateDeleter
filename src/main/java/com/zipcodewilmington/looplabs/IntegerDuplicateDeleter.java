0d3eeec148c0b926d427dca1db2e5964085624a6
nora <sxj042@gmail.com>
2023-04-04 17:43:20 -0400

    trying

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,34 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    Integer[] intArray;
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+//        Stream<Integer> st = Arrays.stream(intArray);
+        List<Integer> list = Arrays.asList(intArray);
+        List<Integer> listWithout = list.stream()
+                .distinct()
+                .collect(Collectors.toList());
+        return listWithout.toArray(new Integer[0]);
+
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        return new Integer[0];
+    }
 }
